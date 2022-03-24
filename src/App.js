ff362d3bbb1b27786ddaa865d4f089b9b9d3e1a0
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-24 19:06:40 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.css';
-import Navbar from'./Components/Navbar';
+import Navbar from './Components/Navbar';
 export default function App() {
+  const webTitle = 'My Web';
   return (
     <>
-      <Navbar/>
+      <Navbar title={webTitle} />
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          {props.title}
         </a>
         <button
           className="navbar-toggler"
